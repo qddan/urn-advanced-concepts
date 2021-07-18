d10422572fe3b4de16a3d986caeb7004f9cb934d
Quang Dong <quangdong211@gmail.com>
2021-07-18 23:54:57 +0700

    Applying animation styling

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
-import { Animated, PanResponder, View } from 'react-native';
+import { Animated, PanResponder, View, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 export default function Deck(props) {
   const position = new Animated.ValueXY();
@@ -10,15 +14,64 @@ export default function Deck(props) {
       //   console.log(gesture);
       position.setValue({ x: gesture.dx, y: gesture.dy });
     },
-    onPanResponderRelease: () => {},
+    onPanResponderRelease: (event, gesture) => {
+      if (gesture.dx > SWIPE_THRESHOLD) {
+        forceSwipe('right');
+      } else if (gesture.dx < -SWIPE_THRESHOLD) {
+        forceSwipe('left');
+      } else {
+        resetPosition();
+      }
+    },
   });
 
-  function renderCards() {
-    return props.data.map((item) => props.renderCard(item));
+  function forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+    Animated.timing(position, {
+      toValue: { x, y: 0 },
+      duration: SWIPE_OUT_DURATION,
+      useNativeDriver: false,
+    }).start(() => onSwipeComplete(direction));
   }
-  return (
-    <Animated.View style={position.getLayout()} {...panResponder.panHandlers}>
-      {renderCards()}
-    </Animated.View>
-  );
+
+  function onSwipeComplete(direction) {
+    const { onSwipeRight, onSwipeLeft } = props;
+
+    // direction === 'right' ? onSwipeRight() : onSwipeLeft();
+  }
+
+  function resetPosition() {
+    Animated.spring(position, {
+      toValue: { x: 0, y: 0 },
+      useNativeDriver: false,
+    }).start();
+  }
+
+  function getCardStyle() {
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH, 0, SCREEN_WIDTH],
+      outputRange: ['-90deg', '0deg', '90deg'],
+    });
+
+    return { ...position.getLayout(), transform: [{ rotate }] };
+  }
+
+  function renderCards() {
+    return props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={getCardStyle()}
+            {...panResponder.panHandlers}
+          >
+            {props.renderCard(item)}
+          </Animated.View>
+        );
+      }
+
+      return props.renderCard(item);
+    });
+  }
+  return <View>{renderCards()}</View>;
 }
